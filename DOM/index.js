0e8f8e08c63432a0462ee9c60dc34289918187ff
namanseth01 <naman.seth_cs19@gla.ac.in>
2021-12-19 15:50:06 +0530

    edit

--- DOM/index.js
+++ DOM/index.js
@@ -1,1 +1,5 @@
-document.query
+// document.firstElementChild.lastElementChild.lastElementChild.lastElementChild.innerHTML =
+//   "Naman";
+
+// document.querySelector("h1").style.color = "blue";
+alert("hii");
