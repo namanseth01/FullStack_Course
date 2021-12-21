8d10553321d95cd5230950c07470c99203f30d41
namanseth01 <naman.seth_cs19@gla.ac.in>
2021-12-21 20:19:28 +0530

    removed DOM

--- DOM/index.js
+++
@@ -1,5 +0,0 @@
-// document.firstElementChild.lastElementChild.lastElementChild.lastElementChild.innerHTML =
-//   "Naman";
-
-// document.querySelector("h1").style.color = "blue";
-// alert("hii");
